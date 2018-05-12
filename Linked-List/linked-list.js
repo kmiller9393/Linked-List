f17bce2327eb04c8983dd744a03dc71bef18b4fa
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-05-12 16:45:08 -0600

    Add jquery & make all read buttonswork

--- Linked-List/linked-list.js
+++ Linked-List/linked-list.js
@@ -1,19 +1,16 @@
-var websiteTitle = document.querySelector('#website-title');
-var websiteURL = document.querySelector('#website-url');
-var enterButton = document.querySelector('.enter');
-var readButton = document.querySelector('.first-read-button');
-var bookmark = document.querySelector('#bookmark1');
-var deleteButton = document.querySelector('.float-right');
+var websiteTitle = $('#website-title');
+var websiteURL = $('#website-url');
+var enterButton = $('.enter');
+var readButton = $('.float-left');
+var deleteButton = $('.float-right');
 
 
-// websiteTitle.addEventListener('input',);
-// websiteURL.addEventListener('input',);
-// enterButton.addEventListener('click',);
-// deleteButton.addEventListener('click',);
-readButton.addEventListener('click', function () {
-   // //event delegation?
-   // let target = event.target;
-   // addReadClass(target);
-   bookmark.classList.add('read');
+// websiteTitle.on('input',);
+// websiteURL.on('input',);
+// enterButton.on('click',);
+// deleteButton.on('click',);
+
+readButton.on('click', function () {
+   $(this).parent('article').toggleClass('read');
 });
 
