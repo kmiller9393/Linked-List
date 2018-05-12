883e982373807d0b931406b1a7216f83d055f562
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-05-12 15:15:23 -0600

    Add more CSS and begin JS

---
+++ Linked-List/linked-list.js
@@ -0,0 +1,15 @@
+var websiteTitle = document.querySelector('#website-title');
+var websiteURL = document.querySelector('#website-url');
+var enterButton = document.querySelector('.enter');
+// var bookmark1 = document.querySelector('.bookmark-1');
+var readButton = document.querySelector('.float-left');
+var deleteButton = document.querySelector('.float-right');
+
+
+websiteTitle.addEventListener('input',);
+websiteURL.addEventListener('input',);
+enterButton.addEventListener('click',);
+readButton.addEventListener('click', function () {
+  // bookmark1.classList.add('read');
+});
+deleteButton.addEventListener('click',);
